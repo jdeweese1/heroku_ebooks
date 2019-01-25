230ac7a263d241c1a0eec61ce2e6ea12dd1c21ae
Jarod DeWeese <jaroddeweese@gmail.com>
2019-01-24 21:29:30 -0600

    updated dependencies

--- local_settings.py
+++ local_settings.py
@@ -7,6 +7,8 @@ Local Settings for a heroku_ebooks account.
 # Configuration for Twitter API
 ENABLE_TWITTER_SOURCES = True # Fetch twitter statuses as source
 ENABLE_TWITTER_POSTING = True # Tweet resulting status?
+
+
 MY_CONSUMER_KEY = environ.get('TWITTER_CONSUMER_KEY')#Your Twitter API Consumer Key set in Heroku config
 MY_CONSUMER_SECRET = environ.get('TWITTER_CONSUMER_SECRET')#Your Consumer Secret Key set in Heroku config
 MY_ACCESS_TOKEN_KEY = environ.get('TWITTER_ACCESS_TOKEN_KEY')#Your Twitter API Access Token Key set in Heroku config
@@ -33,7 +35,7 @@ WEB_ATTRIBUTES = [{'class': 'example-text'}, {}] # A list of dictionaries contai
 ODDS = 8  # How often do you want this to run? 1/8 times?
 ORDER = 2  # How closely do you want this to hew to sensical? 2 is low and 4 is high.
 
-DEBUG = True  # Set this to False to start Tweeting live
+DEBUG = environ.get('DEBUG', False)  # Set this to False to start Tweeting live
 TWEET_ACCOUNT = ""  # The name of the account you're tweeting to.
 
 #Configuration for Twitter parser. TEST_SOURCE will be re-used as as the corpus location.
