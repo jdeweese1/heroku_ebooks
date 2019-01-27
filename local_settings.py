e3e3973cceac3dfbe0434cc31b1e8c3c512e99bc
Jarod DeWeese <jaroddeweese@gmail.com>
2019-01-27 11:03:46 -0600

    more changes to local_settings

--- local_settings.py
+++ local_settings.py
@@ -3,7 +3,7 @@ import environ
 '''
 Local Settings for a heroku_ebooks account. 
 '''
-root = environ.Path(__file__) -1
+root = environ.Path(__file__) - 1
 env = environ.Env()
 
 BASE_DIR = Path(root())
@@ -13,33 +13,33 @@ if READ_DOT_ENV_FILE is True:
     environ.Env.read_env(str(BASE_DIR.joinpath('.env')))
 
 # Configuration for Twitter API
-ENABLE_TWITTER_SOURCES = True # Fetch twitter statuses as source
-ENABLE_TWITTER_POSTING = True # Tweet resulting status?
+ENABLE_TWITTER_SOURCES = env.bool("ENABLE_TWITTER_SOURCES", default=True)  # Fetch twitter statuses as source
+ENABLE_TWITTER_POSTING = env.bool("ENABLE_TWITTER_POSTING", default=True)  # Tweet resulting status?
 
-MY_CONSUMER_KEY = env('MY_CONSUMER_KEY')#Your Twitter API Consumer Key set in Heroku config
-MY_CONSUMER_SECRET = env('MY_CONSUMER_SECRET')#Your Consumer Secret Key set in Heroku config
-MY_ACCESS_TOKEN_KEY = env('MY_ACCESS_TOKEN_KEY')#Your Twitter API Access Token Key set in Heroku config
-MY_ACCESS_TOKEN_SECRET = env('MY_ACCESS_TOKEN_SECRET')#Your Access Token Secret set in Heroku config
+MY_CONSUMER_KEY = env('MY_CONSUMER_KEY')  # Your Twitter API Consumer Key set in Heroku config
+MY_CONSUMER_SECRET = env('MY_CONSUMER_SECRET')  # Your Consumer Secret Key set in Heroku config
+MY_ACCESS_TOKEN_KEY = env('MY_ACCESS_TOKEN_KEY')  # Your Twitter API Access Token Key set in Heroku config
+MY_ACCESS_TOKEN_SECRET = env('MY_ACCESS_TOKEN_SECRET')  # Your Access Token Secret set in Heroku config
 
 # Configuration for Mastodon API
-ENABLE_MASTODON_SOURCES = False # Fetch mastodon statuses as a source?
-ENABLE_MASTODON_POSTING = False # Toot resulting status?
+ENABLE_MASTODON_SOURCES = env.bool("ENABLE_MASTODON_SOURCES", default=False)  # Fetch mastodon statuses as a source?
+ENABLE_MASTODON_POSTING = env.bool("ENABLE_MASTODON_POSTING", default=False)  # Toot resulting status?
 MASTODON_API_BASE_URL = ""  # an instance url like https://botsin.space
 CLIENT_CRED_FILENAME = ''  # the MASTODON client secret file you created for this project
 USER_ACCESS_FILENAME = ''   # The MASTODON user credential file you created at installation.
 
 # Sources (Twitter, Mastodon, local text file or a web page)
-TWITTER_SOURCE_ACCOUNTS = env.list('TWITTER_SOURCE_ACCOUNTS', default=['jaroddeweese', "hotdogsladies"])  # A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
-MASTODON_SOURCE_ACCOUNTS = [""] # A list, e.g. ["@user@instance.tld"]
+TWITTER_SOURCE_ACCOUNTS = env.list('TWITTER_SOURCE_ACCOUNTS', default=['jaroddeweese', 'hotdogsladies', 'zarcasticness', 'GeritWag'])  # A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
+MASTODON_SOURCE_ACCOUNTS = [""]  # A list, e.g. ["@user@instance.tld"]
 SOURCE_EXCLUDE = r'^$'  # Source tweets that match this regexp will not be added to the Markov chain. You might want to filter out inappropriate words for example.
 
-STATIC_TEST = env('STATIC_TEST', default=True)  # Set this to True if you want to test Markov generation from a static file instead of the API.
+STATIC_TEST = env('STATIC_TEST', default=False)  # Set this to True if you want to test Markov generation from a static file instead of the API.
 TEST_SOURCE = env('TEST_SOURCE', default="testcorpus.txt")  # The name of a text file of a string-ified list for testing. To avoid unnecessarily hitting Twitter API. You can use the included testcorpus.txt, if needed.
 
-SCRAPE_URL = False  # Set this to true to scrape a webpage.
+SCRAPE_URL = env.bool("SCRAPE_URL", default=False)  # Set this to true to scrape a webpage.
 SRC_URL = ['http://www.example.com/one', 'https://www.example.com/two']  # A comma-separated list of URLs to scrape
 WEB_CONTEXT = ['span', 'h2']  # A comma-separated list of the tag or object to search for in each page above.
-WEB_ATTRIBUTES = [{'class': 'example-text'}, {}] # A list of dictionaries containing the attributes for each page.
+WEB_ATTRIBUTES = [{'class': 'example-text'}, {}]  # A list of dictionaries containing the attributes for each page.
 
 ODDS = env.int('ODDS', default=8)  # How often do you want this to run? 1/8 times?
 ORDER = 2  # How closely do you want this to hew to sensical? 2 is low and 4 is high.
@@ -47,6 +47,6 @@ ORDER = 2  # How closely do you want this to hew to sensical? 2 is low and 4 is 
 DEBUG = env('DEBUG', default=True)  # Set this to False to start Tweeting live
 TWEET_ACCOUNT = ""  # The name of the account you're tweeting to.
 
-#Configuration for Twitter parser. TEST_SOURCE will be re-used as as the corpus location.
-TWITTER_ARCHIVE_NAME = "tweets.csv" #Name of your twitter archive
-IGNORE_RETWEETS = env('IGNORE_RETWEETS', default=True) #If you want to remove retweets
+# Configuration for Twitter parser. TEST_SOURCE will be re-used as as the corpus location.
+TWITTER_ARCHIVE_NAME = env('TWITTER_ARCHIVE_NAME', default="tweets.csv")  # Name of your twitter archive
+IGNORE_RETWEETS = env('IGNORE_RETWEETS', default=True)  # If you want to remove retweets
