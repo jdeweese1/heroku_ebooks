1ab293f9ebe371b45b27c871b28335f498b6f74b
Jarod DeWeese <jaroddeweese@gmail.com>
2019-02-03 13:27:54 -0600

    Add naughty words to filter out

--- local_settings.py
+++ local_settings.py
@@ -36,7 +36,7 @@ USER_ACCESS_FILENAME = ''   # The MASTODON user credential file you created at i
 # Sources (Twitter, Mastodon, local text file or a web page)
 TWITTER_SOURCE_ACCOUNTS = env.list('TWITTER_SOURCE_ACCOUNTS', default=['jaroddeweese', 'hotdogsladies', 'zarcasticness', 'GeritWag'])  # A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
 MASTODON_SOURCE_ACCOUNTS = [""]  # A list, e.g. ["@user@instance.tld"]
-SOURCE_EXCLUDE = r'^$'  # Source tweets that match this regexp will not be added to the Markov chain. You might want to filter out inappropriate words for example.
+SOURCE_EXCLUDE = r'^(fuck|cunt|whore|shit)$'  # Source tweets that match this regexp will not be added to the Markov chain. You might want to filter out inappropriate words for example.
 
 STATIC_TEST = env('STATIC_TEST', default=False)  # Set this to True if you want to test Markov generation from a static file instead of the API.
 TEST_SOURCE = env('TEST_SOURCE', default="testcorpus.txt")  # The name of a text file of a string-ified list for testing. To avoid unnecessarily hitting Twitter API. You can use the included testcorpus.txt, if needed.
